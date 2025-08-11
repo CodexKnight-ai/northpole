d85ec13eddf9f491afd22f0d3d75e482c3b83eb0
DataWizard1631 <143613914+Deep-DTwiz1631@users.noreply.github.com>
2025-08-11 20:59:11 +0530

    Update Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,13 @@
 import Hero from "./components/Hero"; 
 import About from "./components/About"; 
-import Milestones from "./components/Milestones";
-import Services from "./components/Services";
-import Testinomials from "./components/Testinomials";
+
 
 export default function Home() {
 return (  
   <>
     < Hero /> 
     <About /> 
-    {/* <Milestones />
-    <Services />
-    <Testinomials /> */}
+  
   </>
   );
 }
