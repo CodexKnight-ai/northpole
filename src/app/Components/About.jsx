dd86f78bc3f668dd769a8edcd05e2ae176c49c40
DataWizard1631 <143613914+Deep-DTwiz1631@users.noreply.github.com>
2025-08-23 10:46:57 +0530

    added responsiveness

--- src/app/Components/About.jsx
+++ src/app/Components/About.jsx
@@ -1,45 +1,73 @@
 "use client";
 
-
 export default function About() {
     return (
-        <section className="flex items-center justify-center flex-col px-50 mb-40">
-            <div className="w-[90%] flex  h-[70vh] gap-4">
-                <div className="w-1/3 flex flex-col gap-8 h-full">
-                    <h1 className="text-4xl text-white font-bold">Kalpesh Patel</h1>
-                    <p className="text-[#414141] text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam id optio animi nulla dolores modi hic possimus quisquam! Fugiat doloremque deleniti suscipit dicta consectetur tempora delectus animi. Impedit incidunt officiis obcaecati hic sapiente veniam vitae ipsa quas, ad fugiat quis.</p>
-                </div>
-                <div className="w-1/3 bg-white rounded-xl p-6 h-full flex flex-col gap-4">
-                    <div className="h-[20%]"></div>
-                    <div className="flex flex-col gap-2">
-                        <h1 className="text-4xl text-col1 font-bold">23 Years +</h1>
-                        <p className="text-[#414141] text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est officiis labore enim, voluptas distinctio molestiae autem quis ab dicta a!</p>
+        <section className="w-full py-16 md:py-24 lg:py-32 bg-black">
+            <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-8">
+                    {/* Left Column */}
+                    <div className="flex flex-col gap-6 lg:gap-8 p-6 lg:p-8">
+                        <h1 className="text-3xl md:text-4xl font-bold text-white">Kalpesh Patel</h1>
+                        <p className="text-gray-400 text-base md:text-lg">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam id optio animi nulla dolores modi hic possimus quisquam! Fugiat doloremque deleniti suscipit dicta consectetur tempora delectus animi.
+                        </p>
                     </div>
-                    <div className="bg-gray-700 w-full h-[80%] rounded-xl">
-                        <img src="./planet.jpg" alt="" className="rounded-lg"/>
-                    </div>
-                </div>
-                <div className="w-1/3 rounded-xl p-6 h-full flex flex-col justify-between">
-                    <div className=" w-full h-[40%] rounded-xl p-8">
-                        <img src="./planet.jpg" alt="" className="rounded-lg"/>
-                    </div>
-                    
-                    <div className="flex flex-col gap-2">
-                        <div className="flex flex-row gap-7">
-                            <span className=" rounded-full w-[40px] h-[40px] flex justify-center items-center text-xl text-black bg-white">+</span> 
-                            <span className="text-[#414141] text-md">Lorem woksd dajkd s dajdksdsd Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat molestiae totam. Quos.</span>
+
+                    {/* Middle Column */}
+                    <div className="bg-white rounded-2xl p-6 flex flex-col gap-6">
+                        <div className="flex-1 flex flex-col justify-between gap-6">
+                            <div>
+                                <h1 className="text-3xl md:text-4xl font-bold text-black">23 Years +</h1>
+                                <p className="text-gray-600 mt-2 text-base md:text-lg">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est officiis labore enim, voluptas distinctio molestiae autem quis ab dicta a!
+                                </p>
+                            </div>
+                            <div className="aspect-[4/3] w-full rounded-xl overflow-hidden bg-gray-100">
+                                <img 
+                                    src="/planet.jpg" 
+                                    alt="" 
+                                    className="w-full h-full object-cover"
+                                    onError={(e) => {
+                                        e.target.onerror = null;
+                                        e.target.src = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHZpZXdCb3g9IjAgMCA4MDAgNDAwIiBwcmVzZXJ2ZUFzcGVjdFJhdGlvPSJub25lIj48cmVjdCB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSIjZjFmMWYxIi8+PHRleHQgeD0iNTAlIiB5PSI1JSIgZm9udC1mYW1pbHk9IkFyaWFsIiBmb250LXNpemU9IjE2IiB0ZXh0LWFuY2hvcj0ibWlkZGxlIiBhbGlnbm1lbnQtYmFzZWxpbmU9Im1pZGRsZSIgZmlsbD0iIzY2NiI+UGxhY2Vob2xkZXIgSW1hZ2U8L3RleHQ+PC9zdmc+'
+                                    }}
+                                />
+                            </div>
                         </div>
-                        <div className="flex flex-row gap-7">
-                            <span className=" rounded-full w-[40px] h-[40px] flex justify-center items-center text-xl text-black bg-white">+</span> 
-                            <span className="text-[#414141] text-md">Lorem woksd dajkd s dajdksdsd Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat molestiae totam. Quos.</span>
+                    </div>
+
+                    {/* Right Column */}
+                    <div className="flex flex-col gap-6 p-6 lg:p-8">
+                        <div className="aspect-square w-full max-w-xs mx-auto rounded-2xl overflow-hidden bg-gray-100">
+                            <img 
+                                src="/planet.jpg" 
+                                alt="" 
+                                className="w-full h-full object-cover"
+                                onError={(e) => {
+                                    e.target.onerror = null;
+                                    e.target.src = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHZpZXdCb3g9IjAgMCA4MDAgNDAwIiBwcmVzZXJ2ZUFzcGVjdFJhdGlvPSJub25lIj48cmVjdCB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSIjZjFmMWYxIi8+PHRleHQgeD0iNTAlIiB5PSI1JSIgZm9udC1mYW1pbHk9IkFyaWFsIiBmb250LXNpemU9IjE2IiB0ZXh0LWFuY2hvcj0ibWlkZGxlIiBhbGlnbm1lbnQtYmFzZWxpbmU9Im1pZGRsZSIgZmlsbD0iIzY2NiI+UGxhY2Vob2xkZXIgSW1hZ2U8L3RleHQ+PC9zdmc+'
+                                }}
+                            />
                         </div>
                         
+                        <div className="space-y-4">
+                            {[
+                                "Lorem woksd dajkd s dajdksdsd Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat molestiae totam. Quos.",
+                                "Lorem woksd dajkd s dajdksdsd Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat molestiae totam. Quos."
+                            ].map((text, index) => (
+                                <div key={index} className="flex gap-4 items-start">
+                                    <span className="flex-shrink-0 mt-1 rounded-full w-8 h-8 md:w-10 md:h-10 flex items-center justify-center text-lg md:text-xl text-black bg-white">
+                                        +
+                                    </span>
+                                    <p className="text-gray-400 text-sm md:text-base">
+                                        {text}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                   
                 </div>
             </div>
-            
-
         </section>
     );
 }
